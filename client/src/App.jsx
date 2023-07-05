10d68b8fa7e0ab2ed7e25e35ac5c110a9bb259bc
javier marquez <hello.jmarquez@gmail.com>
2023-07-05 17:06:28 -0500

    menu bootom done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
+import MenuBtm from './Components/MenuBtm/MenuBtm'
 import MenuTop from './Components/MenuTop/MenuTop'
 
 function App() {
 	return (
 		<>
-			<div>
+			<div className='flex flex-col items-center'>
 				<MenuTop />
 				<h1 className='text-3xl font-bold underline text-blue-400'>
 					Hello world!
 				</h1>
+				<MenuBtm />
 			</div>
 		</>
 	)
