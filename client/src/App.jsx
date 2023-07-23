f064b00c64564b3dcaa06580591eb67cf5b3d9bd
Facundo Luna <facundolunaok@gmail.com>
2023-07-22 22:43:15 -0300

    ya casi xd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home, Login, Register, Preferences, Swipe } from './Views'
 import Chat from './Components/Chat/Chat';
-import { useContext } from "react";
-import { AuthContext } from "./Context/AuthContext";
+import { useEffect, useState } from "react";
+import Notification from './Components/PushNotification/Notification';
 
 function App() {
-	const { user } = useContext(AuthContext);
+	const [token, setToken] = useState(null);
+	useEffect(() => {
+		const token = sessionStorage.getItem("token");
+
+		setToken(JSON.parse(token));
+	}, [token]);
 
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/login" element={user ? <Swipe /> : <Login />} />
-			<Route path="/chat" element={user ? <Chat /> : <Login />} />
-			<Route path="/register" element={user ? <Swipe /> : <Register />} />
-			<Route path="/preferences" element={user ? <Preferences /> : <Login />} />
-			<Route path="/swipe" element={user ? <Swipe /> : <Login />} />
+			<Route path="/login" element={token ? <Swipe /> : <Login />} />
+			<Route path="/chat" element={token ? <Chat /> : <Login />} />
+			<Route path="/register" element={token ? <Swipe /> : <Register />} />
+			<Route path="/preferences" element={token ? <Preferences /> : <Login />} />
+			<Route path="/swipe" element={token ? <Swipe /> : <Login />} />
 			<Route path="*" element={<Navigate replace={true} to="/" />} />
+			<Route path="/notication" element={<Notification />} />
 		</Routes>
 	)
 }
--- client/src/Views/Login.jsx
+++ client/src/Views/Login.jsx
@@ -1,15 +1,67 @@
-import { useContext } from "react"
-import { AuthContext } from "../Context/AuthContext"
+import { useState } from "react"
+import { Controller, useForm } from "react-hook-form";
+import FormElement from "../Components/FormElement";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  const {
-    loginUser,
-    loginError,
-    loginInfo,
-    updateLoginInfo,
-    isLoginLoading
-  } = useContext(AuthContext);
+  const { control, handleSubmit, formState: { errors, isLoading } } = useForm({
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
 
+  const navigate = useNavigate();
+
+  const loginUser = async (data) => {
+    try {
+      const api = "https://buddyup.azurewebsites.net/api/auth/login";
+      const response = await axios.post(api, data, {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "*/*",
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success(
+          "¡Bienvenido! ",
+          {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          }
+        );
+        navigate("/swipe");
+        sessionStorage.setItem("token", JSON.stringify(response.data.token));
+      };
+
+    } catch (error) {
+      if (error.message === "Request failed with status code 400") {
+        toast.error(
+          "¡Datos de acceso incorrectos! ",
+          {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          }
+        );
+      };
+    }
+  }
   return (
     <>
       <div className="bg-main_color min-h-screen flex flex-col justify-center">
@@ -18,31 +70,59 @@ const Login = () => {
           <p className="font-nunitosans text-center text-sm w-[358px] mx-auto mb-6">
             Conecta con quien tu quieras, aprende y comparte experiencias y habilidades.
           </p>
-          <form onSubmit={loginUser} className="w-[358px] mx-auto divide-y divide-gray-200">
+          <form onSubmit={handleSubmit(loginUser)} className="w-[358px] mx-auto divide-y divide-gray-200">
             <div className="mt-6">
-              <label className="font-nunitosans font-bold text-base text-[#2f2f2f] pb-1 block">Correo electrónico</label>
-              <input
-                onChange={(e) => updateLoginInfo({ ...loginInfo, email: e.target.value })}
-                type="email" placeholder="Ejemplo@gmail.com" className="h-10 border rounded-lg px-3 py-2 mt-1 mb-5 text-neutral-950 text-sm w-full" />
-              <label className="font-nunitosans font-bold text-base text-[#2f2f2f] pb-1 block">Contraseña</label>
-              <input
-                onChange={(e) => updateLoginInfo({ ...loginInfo, password: e.target.value })}
-                type="password" autoComplete="on" placeholder="Password" className="h-10 border rounded-lg px-3 py-2 mt-1 mb-5 text-neutral-950 text-sm w-full" />
+              <Controller
+                name="email"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <FormElement
+                    type="email"
+                    label="Correo electrónico"
+                    placeholder="Ejemplo@gmail.com"
+                    fieldref={field}
+                    hasError={errors.email?.type === "required"}
+                  />
+                )}
+              />
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <FormElement
+                    type="password"
+                    label="Contraseña"
+                    fieldref={field}
+                    hasError={errors.password?.type === "required"}
+                  />
+                )}
+              />
               <p className="font-nunitosans text-xs mb-4">¿Se te olvidó tu contraseña?</p>
               <button type="submit" className="transition duration-200 bg-accent2 text-black h-12 w-full p-4 rounded-2xl text-xs shadow-sm hover:shadow-md font-nunitosans text-center font-bold inline-block">
                 {
-                  isLoginLoading ? <span>Registrandote...</span> : <span>Continuar</span>
+                  isLoading ? <span>Registrandote...</span> : <span>Continuar</span>
                 }
               </button>
-              {
-                loginError?.error && <div className="w-full mt-2 py-1 rounded-lg text-center bg-red-300 text-red-900">{loginError?.message}</div>
-              }
             </div>
           </form>
           <p className="font-nunitosans text-xs text-center mt-6 text-[#2f2f2f]">¿Aún no tienes una cuenta? Inscribirse.</p>
           <p className="font-nunitosans text-xs text-center text-[#2f2f2f]">Al continuar, reconoce y acepta nuestra Política de privacidad y Términos de uso.</p>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   )
 }
