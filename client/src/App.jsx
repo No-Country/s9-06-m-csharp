86e1c6f455288fcd226a6a183067cae85401738b
Ignacio Benitez <82918800+ignacio2194@users.noreply.github.com>
2023-07-24 22:18:48 -0300

    Revert "Ignacio chat"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home, Login, Register, Preferences, Swipe } from './Views'
-// import Chat from '../src/Components/Chat/conversations/Chat/Chat';
-import Chat from '../src/Components/Conversations/Chat/Chat'
-import { useContext } from 'react'
-import { AuthContext } from './Context/AuthContext'
 import Conversations from './Components/Chat/conversations/conversations'
 // import Chat from './Components/Chat/Chat';
 import { useEffect, useState } from "react";
@@ -20,9 +16,8 @@ function App() {
 			<Route path='/' element={<Home />} />
 			<Route path='/login' element={token ? <Swipe /> : <Login />} />
 			<Route
-				path='/Chat'
-				element={user ? <Chat /> : <Login />}
-				
+				path='/conversations'
+				element={token ? <Conversations /> : <Login />}
 			/>
 			<Route path='/register' element={token ? <Swipe /> : <Register />} />
 			<Route path='/preferences' element={token ? <Preferences /> : <Login />} />
