d10cddb0d2313bf52274ccf10329c968b2b89b4f
javier marquez <hello.jmarquez@gmail.com>
2023-07-26 19:29:05 -0500

    nav bottom

--- client/src/Components/MenuBtm/MenuBtm.jsx
+++ client/src/Components/MenuBtm/MenuBtm.jsx
@@ -7,12 +7,12 @@ import { MdShare } from 'react-icons/md'
 const MenuBtm = () => {
 	return (
 		<nav className='bg-gray-950 flex justify-between w-11/12 rounded-3xl mt-auto mb-1 bottom-0 lg:hidden'>
-			<NavLink to='/'>
+			<NavLink to='#'>
 				<button className='group flex flex-col items-center justify-center w-16 h-16'>
 					<MdShare className='text-white text-2xl' />
 				</button>
 			</NavLink>
-			<NavLink to='/'>
+			<NavLink to='/swipe'>
 				<button className='group flex flex-col items-center justify-center w-16 h-16'>
 					<AiOutlineStar className='text-white text-2xl' />
 				</button>
@@ -22,7 +22,7 @@ const MenuBtm = () => {
 					<BsChatDots className='text-white text-2xl' />
 				</button>
 			</NavLink>
-			<NavLink to='/'>
+			<NavLink to='#'>
 				<button className='group flex flex-col items-center justify-center w-16 h-16'>
 					<RxPerson className='text-white text-2xl' />
 				</button>
